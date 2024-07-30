6bdab3afb406f61f74d42415ac306b8c2ff0a855
Guilherme Melo <guilherme96melo@gmail.com>
2024-07-30 14:48:13 +0200

    creacion de repositorio con la clase madre vehiculo_item

---
+++ vehiculo/vehiculo_item.py
@@ -0,0 +1,9 @@
+class Vehiculo:
+    def __init__(self, marca, modelo, ligado = False):
+        self.marca = marca
+        self.modelo = modelo
+        self._ligado =ligado
+    
+    def __str__(self):
+        return (f'{self.marca} | {self.modelo} | {self._ligado}')
+        
